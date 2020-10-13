3fe6880e76ec18741f419ec66c27271686889bdc
arslava <sarkhangelskiy@sierrawireless.com>
2020-10-12 23:25:44 -0400

    update

--- t.sh
+++ t.sh
@@ -1,12 +1,16 @@
 function isRelease() {
+    GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
     if [[ ("$GIT_BRANCH" == "master" || "$GIT_BRANCH" == "HEAD" ) ]]; then
         GIT_TAG_RN=$(git tag --points-at HEAD | head -n 1)
         if [[ ! -z "$GIT_TAG_RN" && ! "$GIT_TAG_RN" =~ -rc[0-9] ]]; then
-            return true
+            return 1
         fi
     fi 
-    return false  
+    return 0  
 }
 isRelease
 var=$?
 echo $var
+if [[ "$var" == 1 ]]; then 
+	echo GOOD
+fi
